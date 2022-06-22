f6f9eaaf7b1da49e72e85ce4d83bb833041d0c99
wiliamdarmawan <generousbil@gmail.com>
2022-06-22 21:05:36 +0700

    setState and secondary Callback

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: 'Wiliam', lastName: 'Darmawan'},
-      company: "Unit4"
+      name: { firstName: "Wiliam", lastName: "Darmawan" },
+      company: "Unit4",
     };
   }
 
@@ -19,13 +19,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work 
-            at {this.state.company} 
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
           </p>
           <button
             onClick={() => {
-              this.setState({ name: { firstName: "Testing", lastName: "Test" } });
-              console.log(this.state);
+              this.setState( () => {
+                return {
+                  name: { firstName: "Testing", lastName: "Test" },
+                } // object which we want the result to be changed into
+              }, 
+              () => {
+                  console.log(this.state);
+                }
+              );
             }}
           >
             Change Name
