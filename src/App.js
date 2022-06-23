bd809a19d43929ec0b749dafedb4a7a915d7d968
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-06-23 16:30:30 +0700

    CardList Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
+import CardList from './components/card-list/card-list.component'
 import "./App.css";
 
 class App extends Component {
@@ -60,13 +60,14 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChanged}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList/>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class CardList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello I'm the CardList Component</h1>
+        <div>
+          <p>text</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardList;
