f6258daa94656157d34f2c25592372e47217b480
Wiliam Darmawan <Wiliam.Darmawan@unit4.com>
2022-06-24 18:12:22 +0700

    Remove Class Components

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const App = () => {
       monster.name.toLowerCase().includes(searchField)
     );
     setFilteredMonsters(newFilteredMonsters);
-    console.log("effect is firing");
   }, [monsters, searchField]);
 
   const onSearchChanged = (event) => {
@@ -42,59 +41,5 @@ const App = () => {
     </div>
   );
 };
-
-/* class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
-
-  componentDidMount() {
-    // Run when the component gets Mounted
-    // Mount -> first time React renders, it is considered as "Mounted"
-    // Component will only be re-mounted if it's been un-mounted
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
-
-  onSearchChanged = (event) => {
-    const searchField = event.target.value.toLowerCase();
-
-    this.setState(() => {
-      return { searchField };
-    });
-  };
-
-  render() {
-
-    const { monsters, searchField } = this.state;
-    const { onSearchChanged } = this;
-
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField)
-    );
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="monsters-search-box"
-          onChangedHandler={onSearchChanged}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-} */
 
 export default App;
